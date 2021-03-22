0325d9ccd991df50ca6c8daa4e4894de6d2074bd
shrikant <skanthale9@gmail.com>
2021-03-22 01:57:40 -0700

    discuss about array and arrays some concept

---
+++ array.js
@@ -0,0 +1,71 @@
+/*let arr = [45,75,85,15,65]
+let max = arr[0]
+
+for(let i=0; i<=arr.length; i++){
+    if(max<=arr[i]){
+        max = arr[i];
+    }
+}
+console.log("max number of array is : " +max);
+*/
+
+/*let arr = [50,40,70,30,90,30]
+let min = arr[0]
+for(let i=0 ; i<=arr.length ; i++){
+    if(min>=arr[i]){
+        min = arr[i];
+    }
+}
+console.log("min number of array is : " +min)
+*/
+/*
+let arr = [45,75,85,25,35,65,95]
+let temp = 50
+for(let i = 0 ; i<=arr.length ; i++){
+    if(temp<=arr[i]){
+        console.log(arr[i])
+    }
+}
+*/
+/*
+let arr = [45,75,85,25,35,65,95]
+let index = arr.indexOf(15);
+if(arr[index]){
+console.log(arr[index]+" "+index);
+}
+else{
+    console.log("Error!!")
+}
+*/
+
+// search element in array
+/*
+let arr = [45,75,85,25,35,65,95]
+let index = arr.indexOf(25);
+if(arr[index]){
+console.log(arr[index]+" is the number of array found on "+index+ " index number");
+}
+else{
+    console.log("Error!!")
+}
+*/
+
+/*
+let arr = ["a","e","i","o","u"]
+let index = arr.indexOf("o")
+if(arr[index]){
+    console.log(arr[index])
+}
+else {
+    console.log("error!!")
+}
+*/
+
+let arrName = [2,3,5,7,11,13,17,19]
+let index = arrName.indexOf(13)
+if(arrName[index]){
+    console.log(arrName[index]+" "+index);
+}
+else{
+    console.log("error!!!")
+}

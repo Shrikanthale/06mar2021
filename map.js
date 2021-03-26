46980f8d40ef6f41e9607f74e06d65a7eca1328e
shrikant <skanthale9@gmail.com>
2021-03-26 08:30:12 -0700

    discuss about map function in js

---
+++ map.js
@@ -0,0 +1,18 @@
+/*function sqr(number) {
+    return number*number 
+}
+
+var arrnum = [1,2,3,4,5,6,7,8,9]
+var result = arrnum.map(sqr)
+console.log(result)
+*/
+
+
+function cube(num) {
+    return num*num*num
+    
+}
+
+var arrcube = [1,2,3,4,5,6,7,8,9,]
+let result = arrcube.map(cube)
+console.log("cube os 1 to 9 numbers " +result)
